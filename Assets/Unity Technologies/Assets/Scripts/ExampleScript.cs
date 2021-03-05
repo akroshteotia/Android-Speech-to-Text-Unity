a9e980f32435937ef901c8facd140b9a6aa37e2c
Akrosh Teotia <akrosh.teotia@quytech.com>
2021-03-05 20:47:37 +0530

    Unity Speech to text android plugin

---
+++ Assets/Unity Technologies/Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Linq;
+using MiniJSON;
+using System.Collections.Generic;
+
+public class ExampleScript : MonoBehaviour {
+
+    public Text uiText;
+    IList<object> result;
+    // Use this for initialization
+    void Start () {
+        ListnerManager._instance.resultCallBack = OnRecievedResult;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnRecievedResult(string audioOutput)
+    {
+        result = MiniJSON.Json.Deserialize(audioOutput) as IList<object>;
+        string[] answer = result.Cast<string>().ToArray();
+        Debug.Log(answer);
+        //answer will return an arry of words... those are similar to your word 
+        // i am taking only first word.. you can check array in log if there any
+        uiText.text = "";
+        foreach (string voice in answer)
+            uiText.text += voice + "\t";
+    }
+
+    public void BtnStartSpeech()
+    {
+        Debug.Log("start");
+        ListnerManager._instance.StartSpeech();
+        
+    }
+
+    public void BtnStopSpeech()
+    {
+        Debug.Log("stop");
+        ListnerManager._instance.StopSpeech();
+    }
+
+    
+}
---
+++ Assets/Unity Technologies/Assets/Scripts/ListnerManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+
+public class ListnerManager : MonoBehaviour
+{
+    public static ListnerManager _instance;
+    public System.Action<string> resultCallBack;
+    public void Result(string dataReceived)
+    {
+        Debug.Log(dataReceived);
+        if (resultCallBack != null)
+        {
+            resultCallBack(dataReceived);
+            Debug.Log("result");
+        }
+    }
+
+    void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        Debug.Log(_instance);
+    }
+  
+  
+    void Start()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        using (AndroidJavaClass javaClass = new AndroidJavaClass("com.voice.plugin.UnityBridge"))
+        {
+            javaClass.CallStatic("Init");
+            Debug.Log("Init()");
+        }
+#endif
+    }
+
+    public void StartSpeech()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        // This function calling Java Code from Jar file to start android Speech Service
+        using (AndroidJavaClass javaClass = new AndroidJavaClass("com.voice.plugin.UnityBridge"))
+        {
+            javaClass.CallStatic("StartSpeech");
+        }
+#endif
+    }
+
+    public void StopSpeech()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        // This function calling Java Code from Jar file to start android stop Service
+        using (AndroidJavaClass javaClass = new AndroidJavaClass("com.voice.plugin.UnityBridge"))
+        {
+            javaClass.CallStatic("StopSpeech");
+        }
+#endif
+    }
+}
